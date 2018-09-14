2df346c2c990c60c9ae88d3f20f7d77e4fb08e1a
dimpurr <dimpurr@live.com>
2018-09-14 16:13:53 +0800

    Nuxt Init with Stylus

---
+++ nuxt.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  css: [
+    { src: '~assets/global.styl', lang: 'stylus' }
+  ],
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'byrteam',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'team.byr.moe Index of BYR Team' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
