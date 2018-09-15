7c2e809ec4435288546895ecafe77ac3d0c3b2cf
dimpurr <dimpurr@live.com>
2018-09-15 18:50:07 +0800

    Fin Groups

--- components/GroupsIntro.vue
+++ components/GroupsIntro.vue
@@ -2,18 +2,20 @@
     <section class="GroupsIntro">
 		<ul class="groups">
 			<li class="group" v-for="(group,index) in groups" :key="index">
-				<h3>{{ group.name }}</h3>
-				<h4>{{ group.name_en }}</h4>
-				<p v-html="group.intro"></p>
-				<ul class="links">
-					<li v-for="(link,index) in group.links" :key="index">
-						<a :href="link.url">{{ link.title }}</a>
-					</li>
-				</ul>
+				<div class="info">
+					<h3>{{ group.name }}</h3>
+					<h4>{{ group.name_en }}</h4>
+					<p v-html="group.intro"></p>
+					<ul class="links">
+						<li v-for="(link,index) in group.links" :key="index">
+							<a :href="link.url">{{ link.title }}</a>
+						</li>
+					</ul>
+				</div>
 				<div class="slideCtn">
+					我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide<br>
 					我是 slide<br>
-					我是 slide<br>
-					我是 slide<br>
+					我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide我是 slide<br>
 					我是 slide<br>
 				</div>
 			</li>
@@ -32,7 +34,7 @@ export default {
 			{
 				name: '技术组',
 				name_en: 'DEVELOPE GROUP',
-				intro: 'BYR 坚强的技术后盾，新产品的研发中心。<br>希望热爱技术的小伙伴加入我们一起玩！',
+				intro: 'BYR 坚强的技术后盾，新产品的研发中心。坚强的技术后盾，新产品的研发中心。坚强的技术后盾，新产品的研发中心。<br>希望热爱技术的小伙伴加入我们一起玩！',
 				links: [
 					{
 						title: '计划中项目',
@@ -88,21 +90,66 @@ export default {
 
 <style lang="stylus" scoped>
 .GroupsIntro
-	.groups
+	margin-top 200px
+	ul.groups
 		position relative
 		margin 10px 0
-		.group
-			height 500px
+		padding 0
+		li.group
+			display block
+			position relative
+			height 380px
 			box-sizing border-box
+			&:nth-child(even)
+				.info
+					padding 30px 300px 0 0
+					text-align right 
+				.slideCtn
+					left auto
+					right -130px
+					text-align left
+				&::before
+					left auto
+					right -100px
+					top -150px
+			.info
+				// border 1px solid #000
+				padding 30px 0 0 300px
+				text-align left 
+				h3, h4
+					font-weight normal
+				h3
+					font-size 20px
+				h4
+					font-size 14px
+					color #7d7d7d
+				p
+					margin 20px 0 28px 0
+				ul
+					padding 0
+					li
+						display inline-block
+						a
+							text-decoration none
+							border 1px solid #49b1ff
+							border-radius 9999px
+							padding 6px 18px
+							color #7d7d7d
+							font-size 14px
+							margin-right 16px
 			.slideCtn
+				border 1px solid #000
 				position absolute
-				left -200px
+				width 300px
+				left -130px
+				top 0
+				text-align right 
 				&::before
 					content ''
 					display block
 					position absolute
 					left -100px
-					top -100px
+					top -150px
 					width 500px
 					height 500px
 					background-image url('~static/box.png')
