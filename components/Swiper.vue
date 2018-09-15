e8fbc9d4e25d1d3e369463d75d9ac9ee5e014484
Heymind <heymind@outlook.com>
2018-09-15 11:04:57 +0800

    a simple silder

---
+++ components/Swiper.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="swiper">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="stylus" scoped>
+.swiper
+    display block
+    position relative
+</style>
