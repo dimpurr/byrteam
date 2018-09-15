dd7e91c49fc6c7519f7848566a1b90f6c6da7f28
Heymind <heymind@outlook.com>
2018-09-15 11:21:55 +0800

    btn

---
+++ components/Btn.vue
@@ -0,0 +1,35 @@
+<template>
+    <a class="btn" :class="{flat,outline,large}"><slot /></a>
+</template>
+<script>
+export default {
+    props:['flat','outline','large']
+}
+</script>
+<style lang="sass" scoped>
+.btn
+    font-size: 1rem
+    padding: 1.3rem
+    display: flex
+    width: 6rem
+    justify-content: center
+    align-items: center
+    background: rgb(102, 204, 255)
+    color: white
+.flat
+    background: none !important
+    color: rgb(127, 127, 127)
+
+.outline
+  border-width: 1px
+  border-color: rgb(102, 204, 255)
+  border-style: solid
+  border-radius: 1rem
+  background: none
+  color: rgb(177, 177, 177)
+  padding: 0.6rem
+
+.large
+    width: 6rem
+    height: 3rem
+</style>
